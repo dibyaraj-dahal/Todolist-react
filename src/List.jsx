b0b916463013d47b67a97494d10c806441efe34b
dibyaraj <dahaldibyaraj46@gmail.com>
2026-06-20 23:40:51 +0545

    file removed

--- src/List.jsx
+++
@@ -1,7 +0,0 @@
-function List(){
-    const names = ["Dibya", "Ram", "Shyam", "Hari", "Sita"];
-    names.sort();
-    const listItems = names.map((name) => <li>{name}</li>);
- return(<ol>{listItems}</ol>);
-}
-export default List
