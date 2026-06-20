0dbec0412bc813edf4f281cf1e8584c5bcb8b331
dibyaraj <dahaldibyaraj46@gmail.com>
2026-06-20 23:37:14 +0545

    some files removed

--- src/Button.jsx
+++
@@ -1,9 +0,0 @@
-function Button(){
-const handleClick=(e)=>e.target.textContent="Clicked";
-return(
-   
-      
-    <button className="btn" onDoubleClick={handleClick}>Click Me</button>
-);
-     }
-export default Button
--- src/UserGreeting.jsx
+++
@@ -1,9 +0,0 @@
-function UserGreting(props){
-    return(
-        <div>
-            {props.isLoggedin ? <h1>Welcome {props.username}</h1> : <h1>Welcome Guest</h1>}
-        </div>
-    )
-
-}
-export default UserGreting ; 
